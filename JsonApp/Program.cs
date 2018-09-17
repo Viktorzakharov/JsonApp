a598bde039133f6e1be2149e9229f8a972d12893
Viktorzakharov <Viktorzakharov93@outlook.com>
2018-09-17 22:18:27 +0500

    Десериализация массива Json

--- JsonApp/Program.cs
+++ JsonApp/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace JsonApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var client = new WebClient();
+            client.DownloadFile("https://jsonplaceholder.typicode.com/todos", "FileJson.json");
+
+            var file = new StreamReader("FileJson.json");
+            var jsonArray = JsonConvert.DeserializeObject<List<User>>(file.ReadToEnd());
+
+            var dictionary = new Dictionary<string, int[]>();
+            foreach (var e in jsonArray)
+            {
+                if (!dictionary.ContainsKey(e.UserId))
+                    dictionary[e.UserId] = new int[2];
+                dictionary[e.UserId][0]++;
+                if (e.Completed) dictionary[e.UserId][1]++;
+            }
+
+            Console.WriteLine("{0,-20}{1,-20}{2}", "UserId", "TasksCount", "CompletedTasksAmount");
+            foreach (var e in dictionary)
+                Console.WriteLine("{0,-20}{1,-20}{2}", e.Key, e.Value[0], e.Value[1]);
         }
     }
 }
---
+++ JsonApp/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace JsonApp
+{
+    [DataContract]
+    class User
+    {
+        [DataMember(Name = "userId")]
+        public string UserId { set; get; }
+        [DataMember(Name = "id")]
+        public string Id { set; get; }
+        [DataMember(Name = "title")]
+        public string Title { set; get; }
+        [DataMember(Name = "completed")]
+        public bool Completed { set; get; }
+    }
+}
